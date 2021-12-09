30372eab33ed4d30fae83705b7d4f074d4dc0ca5
sirben181 <bensonkamauben@gmail.com>
2021-12-09 23:26:12 +0300

    here

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import Post from './Post'
 import db from './firebase'
-import { collection,orderBy, query, onSnapshot} from "firebase/firestore"
+import { useCollection } from "react-firebase-hooks/firestore"
+//import { collection,orderBy, query, onSnapshot} from "firebase/firestore"
 // import {} from '@mui/icons-material'
 
 
 // const q = query(citiesRef, orderBy("name"), limit(3));
 const Posts = () => {
-    const [posts,setPosts]=useState([])
-    useEffect(()=>{
-        const q = query(collection(db, "posts"),orderBy("timestamp", "desc"));
-        const unsubscribe= onSnapshot(q, (querySnapshot) => {
-           querySnapshot.forEach((doc) => {
-              const data=doc.data()
-               setPosts([data])
-                  })
-         });
-    },[db])
+    const [realtimePosts,loading,error]=useCollection(
+      db.collection("posts").orderBy("timestamp","desc")
+    );
+    console.log(realtimePosts)
+    // useEffect(()=>{
+    //     const q = query(collection(db, "posts").orderBy("timestamp", "desc"));
+    //     const unsubscribe= onSnapshot(q, (querySnapshot) => {
+    //        querySnapshot.forEach((doc) => {
+    //           const data=doc.data()
+    //            setPosts([data])
+    //               })
+    //      });
+    // },[db])
        
     return (
         <div>
-             {posts.map(post=>(
+             {realtimePosts.docs.map((post)=>(
               <Post 
               username={post.username} 
-              key={post.id}
+              key={post.id} 
               image={post.image}
               posttext={post.message}
               timestamp={post.timestamp}
